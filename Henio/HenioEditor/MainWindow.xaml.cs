3ae9f612d35b848ad013a50a02151a08f49300af
Oskar Kogut <kogutoskar123@gmail.com>
2023-05-08 23:55:42 +0200

    Project browser view

--- Henio/HenioEditor/MainWindow.xaml.cs
+++ Henio/HenioEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HenioEditor.GameProject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,26 @@ namespace HenioEditor
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            var projectBrowser = new ProjectBrowserDialog();
+            if (projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
         }
     }
 }

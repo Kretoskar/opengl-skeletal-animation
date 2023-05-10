1b06307740cd1bd7b1e852fb656b18ae3e83ee7c
Oskar Kogut <kogutoskar123@gmail.com>
2023-05-10 21:09:26 +0200

    ViewModelBase and binding for New Project

---
+++ Henio/HenioEditor/GameProject/NewProject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HenioEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string _name = "NewProject";
+        private string _path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\HenioProject\";
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string Path
+        {
+            get => _path;
+            set
+            {
+                if (_path != value)
+                {
+                    _path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+    }
+}

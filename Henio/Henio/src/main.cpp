2989ff46e5ff3f9b0f0f3e81b54fe64f1cc94c46
Oskar Kogut <kogutoskar123@gmail.com>
2023-06-14 23:39:45 +0200

    add stb_image lib

--- Henio/Henio/src/main.cpp
+++ Henio/Henio/src/main.cpp
@@ -12,13 +12,10 @@ int main()
 {
 	// glm test
 	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-
-
 	glm::mat4 trans = glm::mat4(1.0f);
-	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	vec = trans * vec;
 	std::cout << vec.x << vec.y << vec.z << std::endl;
-
 
 	glfwInit();
 

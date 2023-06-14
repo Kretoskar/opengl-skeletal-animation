ca2a91648925b778f6d95b8dc66ab3a488426112
Oskar Kogut <kogutoskar123@gmail.com>
2023-06-14 23:35:20 +0200

    add and test glm

--- Henio/Henio/src/main.cpp
+++ Henio/Henio/src/main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 void frameBufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 int main()
 {
+	// glm test
+	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+
+
+	glm::mat4 trans = glm::mat4(1.0f);
+	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+	std::cout << vec.x << vec.y << vec.z << std::endl;
+
+
 	glfwInit();
 
 	// using 3.3 as it's the doc supported version

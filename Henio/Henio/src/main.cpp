9ceb45d88ad4a876f6ea660a06ecdcab7d26fc2a
Oskar Kogut <kogutoskar123@gmail.com>
2023-06-15 20:01:37 +0200

    some more fun. Rect drawing, indices buffer

--- Henio/Henio/src/main.cpp
+++ Henio/Henio/src/main.cpp
@@ -97,15 +97,22 @@ int main()
 
 	float vertices[] =
 	{
-		-0.5f, -0.5f, -0.5f,
-		0.0f, 0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f
+		0.5f, 0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+	};
+	uint32_t indices[] =
+	{
+		0, 1, 2,
+		2, 3, 0
 	};
 
 	// vao, vbo
-	uint32_t VAO, VBO;
+	uint32_t VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -114,6 +121,9 @@ int main()
 	// set attribute pointer
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -125,7 +135,8 @@ int main()
 		//draw shapes
 		glBindVertexArray(VAO);
 		glUseProgram(shaderProgram);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// swap color buffer used to render this render iteration
 		// and output it to screen (double buffer doc page 23)

0810a679ba7620ed89c32994aa6b4e54b0d6ab11
Oskar Kogut <kogutoskar123@gmail.com>
2023-07-02 16:53:39 +0200

    Passing vertex bone data to shaders

--- Henio/Henio/assets/vertex_core.glsl
+++ Henio/Henio/assets/vertex_core.glsl
@@ -3,10 +3,15 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoord;
+layout (location = 3) in ivec4 aBoneIds;
+layout (location = 4) in vec4 aWeights;
+
 
 out vec3 FragPos;
 out vec3 Normal;
 out vec2 TexCoord;
+flat out ivec4 BoneIds;
+out vec4 Weights;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -19,4 +24,7 @@ void main()
 	
 	gl_Position = projection * view * vec4(FragPos, 1.0);
 	TexCoord = aTexCoord;
+	
+	BoneIds = aBoneIds;
+	Weights = aWeights;
 }

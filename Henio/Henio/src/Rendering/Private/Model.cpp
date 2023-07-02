46ae989fbe9627af9f27b44be8c7085b3c717a27
Oskar Kogut <kogutoskar123@gmail.com>
2023-07-02 14:12:42 +0200

    Move mesh code to Model files

--- Henio/Henio/src/Rendering/Private/Model.cpp
+++ Henio/Henio/src/Rendering/Private/Model.cpp
@@ -6,7 +6,114 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include "Rendering/Public/AssimpHelper.h"
+#include <assimp/postprocess.h>
+
+std::vector<Vertex> Vertex::GenerateList(float* vertices, int nVertices)
+{
+    std::vector<Vertex> ret(nVertices);
+
+    constexpr int stride = sizeof(Vertex) / sizeof(float);
+
+    for(int i = 0; i < nVertices; i++)
+    {
+        ret[i].pos = glm::vec3(
+            vertices[i * stride + 0],
+            vertices[i * stride + 1],
+            vertices[i * stride + 2]
+        );
+
+        ret[i].normal = glm::vec3(
+            vertices[i * stride + 3],
+            vertices[i * stride + 4],
+            vertices[i * stride + 5]
+        );
+        
+        ret[i].texCoord = glm::vec2(
+            vertices[i * stride + 6],
+            vertices[i * stride + 7]
+        );
+    }
+
+    return ret;
+}
+
+Mesh::Mesh()
+{
+}
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<uint32_t> indices, std::vector<Texture> textures)
+    : vertices(vertices), indices(indices), textures(textures)
+{
+    Setup();
+}
+
+void Mesh::Render(Shader shader)
+{
+    uint32_t diffuseIdx = 0;
+    uint32_t specularIdx = 0;
+
+    for (uint32_t i = 0; i < textures.size(); i++)
+    {
+        glActiveTexture(GL_TEXTURE0 + i);
+        
+        std::string name;
+        switch (textures[i].type) {
+            case aiTextureType_DIFFUSE:
+                name = "diffuse" + std::to_string(diffuseIdx++);
+                break;
+            case aiTextureType_SPECULAR:
+                name = "specular" + std::to_string(specularIdx++);
+                break;
+            default: ;
+        }
+
+        shader.SetInt(name, i);
+        textures[i].Bind();
+    }
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+
+    glActiveTexture(GL_TEXTURE0);
+}
+
+void Mesh::Cleanup()
+{
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+}
+
+void Mesh::Setup()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(uint32_t), &indices[0], GL_STATIC_DRAW);
+
+    //vertex.position
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>((offsetof(Vertex, pos))));
+
+    //vertex.normal
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>((offsetof(Vertex, normal))));
+    
+    //vertex.texcoord
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>((offsetof(Vertex, texCoord))));
+
+    glBindVertexArray(0);
+}
+
 
 Model::Model(glm::vec3 pos, glm::vec3 size)
     : pos(pos), size(size) {}
@@ -15,7 +122,6 @@ void Model::LoadModel(std::string path)
 {
     Assimp::Importer import;
     const aiScene* scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
-    AssimpHelper::ParseScene(scene);
 
     if(!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
     {
--- Henio/Henio/src/Rendering/Public/Model.h
+++ Henio/Henio/src/Rendering/Public/Model.h
@@ -4,9 +4,42 @@
 
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
-#include <assimp/postprocess.h>
 
-#include "Mesh.h"
+#include <vector>
+#include <glm/glm.hpp>
+
+#include "Shader.h"
+#include "Texture.h"
+
+struct Vertex
+{
+    glm::vec3 pos;
+    glm::vec3 normal;
+    glm::vec2 texCoord;
+
+    static std::vector<Vertex> GenerateList(float* vertices, int nVertices);
+};
+
+class Mesh
+{
+public:
+    std::vector<Vertex> vertices;
+    std::vector<uint32_t> indices;
+    uint32_t VAO;
+
+    std::vector<Texture> textures;
+
+    Mesh();
+    Mesh(std::vector<Vertex> vertices, std::vector<uint32_t> indices, std::vector<Texture> textures = {});
+
+    void Render(Shader shader);
+    void Cleanup();
+
+private:
+    uint32_t VBO, EBO;
+
+    void Setup();
+};
 
 class Model
 {

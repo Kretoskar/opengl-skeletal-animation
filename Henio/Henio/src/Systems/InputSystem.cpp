d15a1e690572d19da5776364d11da067e742dae7
Oskar Kogut <kogutoskar123@gmail.com>
2023-09-04 19:43:46 +0200

    WIP switch to ECS

--- Henio/Henio/src/Systems/InputSystem.cpp
+++ Henio/Henio/src/Systems/InputSystem.cpp
@@ -1,1 +1,28 @@
 ﻿#include "InputSystem.h"
+#include "Components/Public/InputComponent.h"
+#include "Input/Public/Keyboard.h"
+#include "Input/Public/Mouse.h"
+
+void InputSystem::Init()
+{
+}
+
+void InputSystem::Update(float deltaTime)
+{
+    for (auto const entity : mEntities)
+    {
+        auto& inputComp = coordinator.GetComponent<InputComponent>(entity);
+
+        inputComp.keyboardKeys = Keyboard::keys;
+        inputComp.keyboardKeysChanged = Keyboard::keysChanged;
+
+        inputComp.mouseX = Mouse::x;
+        inputComp.mouseY = Mouse::y;
+        inputComp.mouseDeltaX = Mouse::deltaX;
+        inputComp.mouseDeltaY = Mouse::deltaY;
+        inputComp.mouseScrollDeltaX = Mouse::scrollDeltaX;
+        inputComp.mouseScrollDeltaY = Mouse::scrollDeltaY;
+        inputComp.mouseButtons = Mouse::buttons;
+        inputComp.mouseButtonsChanged = Mouse::buttonsChanged;
+    }
+}
--- Henio/Henio/src/Systems/InputSystem.h
+++ Henio/Henio/src/Systems/InputSystem.h
@@ -1,26 +1,12 @@
 ﻿#pragma once
-#include "Components/Public/InputComponent.h"
 #include "Core/ECS/Public/Coordinator.h"
 #include "Core/ECS/Public/SystemManager.h"
-#include "Input/Public/Keyboard.h"
 
 extern ECS::Coordinator coordinator;
 
 class InputSystem : public ECS::System
 {
 public:
-    void Init()
-    {
-    }
-    
-    void Update(float deltaTime)
-    {
-        for (auto const entity : mEntities)
-        {
-            auto& inputComp = coordinator.GetComponent<InputComponent>(entity);
-
-            inputComp.keyboardKeys = Keyboard::keys;
-            
-        }
-    }
+    void Init();
+    void Update(float deltaTime);
 };
